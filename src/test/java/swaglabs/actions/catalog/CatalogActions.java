390599cf597267e2f7478597ba381bcea8443db2
John Ferguson Smart <john.smart@wakaleo.com>
2022-04-08 08:18:08 +0100

    Sample solution

--- src/test/java/swaglabs/actions/catalog/CatalogActions.java
+++ src/test/java/swaglabs/actions/catalog/CatalogActions.java
@@ -1,21 +1,22 @@
 package swaglabs.actions.catalog;
 
 import net.serenitybdd.core.steps.UIInteractions;
+import org.openqa.selenium.By;
 
 import java.util.List;
 
 public class CatalogActions  extends UIInteractions {
 
     public List<String> productTitles() {
-        return $$(".inventory_item_name").texts();
+        return findAll(".inventory_item_name").texts();
     }
 
     public List<ProductInfo> products() {
-        return findAll(".inventory_item_description")
+        return findAll(".inventory_item")
                 .map(item -> {
                    String title = item.findBy(".inventory_item_name").getText();
                    String description = item.findBy(".inventory_item_desc").getText();
-                   String image = item.findBy(".img.inventory_item_img").getAttribute("src");
+                   String image = item.find(By.tagName("img")).getAttribute("src");
                    double price = Double.parseDouble(item.findBy(".inventory_item_price").getText().replace("$",""));
 
                    return new ProductInfo(title, description, price, image);
